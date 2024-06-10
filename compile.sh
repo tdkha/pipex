5810ddb5b49bedb6107bee36772aed8279fc92e2
Kha Tieu <ktieu@c1r4p2.hive.fi>
2024-06-10 15:04:40 +0300

    trying to fix sed test case

--- compile.sh
+++ compile.sh
@@ -20,17 +20,32 @@ touch infile
 
 # echo "-------------------------------"
 
+# echo "-------------------------------"
+# ./pipex infile "/bin/ls " "wc -l" outfile
+# cat outfile
+
+# rm -rf outfile
+
+# < infile /bin/ls | wc -l > test
+# cat test
+
+# rm -rf outfile test
+
+# echo "-------------------------------"
+
+
 echo "-------------------------------"
-./pipex infile "/bin/ls " "wc -l" outfile
+./pipex infile 'sed    "s/And/But/"' 'grep But' outfile
 cat outfile
 
 rm -rf outfile
 
-< infile /bin/ls | wc -l > test
+< infile sed "s/And/But/" | grep But > test
 cat test
 
 rm -rf outfile test
 
 echo "-------------------------------"
+
 
 make fclean
